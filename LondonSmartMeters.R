318490345ad7404667c9305e906e440e895e1ab5
Jonathan Farland <jonfarland@Jonathans-Mac-mini.local>
2021-08-18 11:54:49 -0700

    add initial Rmarkdown and jupyter files

---
+++ LondonSmartMeters.R
@@ -0,0 +1,17 @@
+
+
+# Developer Tools
+
+# Data Science Packages
+ds_packages <-
+  c("glmnet", "caret", "neuralnet", "")
+
+################
+# Read in Data #
+################
+
+
+
+############
+# Modeling #
+############
